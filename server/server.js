88a46ebce77e2ecbeb262f055bb4aacc6b6b9e0e
Alex <myspacealexr1der@gmail.com>
2021-08-18 12:21:43 -0400

    bug fixed

--- server/server.js
+++ server/server.js
@@ -2,10 +2,10 @@ const express = require('express');
 const path = require('path');
 const PORT = 3333;
 const cors = require('cors');
-const dbRetriver = require('../models/dbRetriver');
+const dbRetriever = require('../models/dbRetriver');
 const app = express();
 
-app.get('/', dbRetriver, (req, res) => {
+app.get('/', dbRetriever, (req, res) => {
   res.status(200).json({fields:res.locals.db_data, tables: res.locals.db_tables});
 });
 
