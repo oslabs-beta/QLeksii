9d00eb78775272e8d1836654d133fe4a41030e70
Alex <myspacealexr1der@gmail.com>
2021-08-18 11:39:39 -0400

    retrival function is working

--- main.tsx
+++ main.tsx
@@ -1,18 +1,20 @@
 const {app, BrowserWindow} = require('electron');
 const path = require('path');
+require('./server/server.js');
 
 function createWindow(params) {
-    const app = new BrowserWindow({
+    const win = new BrowserWindow({
         width:1200,
         heigh:800,
+        autoHideMenuBar:false,
         backgroundColor: "white",
         webPreferences:{
-            nodeIntegration: false,
+            nodeIntegration: true,
             worldSafeExecuteJavaScript: true,
-            contextIsolation: true
+            contextIsolation: false
         }
     })
-    app.loadFile('index.html');
+    win.loadFile('index.html');
 }
 require('electron-reload')(__dirname, {
     electron: path.join(__dirname, 'node_modules', '.bin', 'electron')
--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,30 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React, { Component }  from "react";
+import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
 import './style/app.scss';
 
 
-const App = () => (
-  <div>
-  <h1> Qleksii is running in chill mode! </h1>
-  <button>Chill</button>
- </div>
+class App extends Component {
+  constructor(){
+  super()
+  this.state={
+    arr: []
+  }
+  }
 
-);
-
+  render() {
+    const clicker=(e)=>{
+      fetch("http://localhost:3333/").then(res => res.json()).then(res=> console.log(res))
+    }
+    return (
+      <div>
+          <h1> Qleksii is running in chill mode! </h1>
+          <button onClick={clicker}>Chill</button>
+      
+      </div>
+    );
+  }
+}
 
 
 export default App;
