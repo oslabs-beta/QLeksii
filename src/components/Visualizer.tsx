a2b97a2c0e95507261d89e1add004a0bbdb6cc33
Damien Evans <dnyceboy@gmail.com>
2021-08-18 14:53:45 -0400

    taking dev changes and adding files and folders. such as components and containers

---
+++ src/components/Visualizer.tsx
@@ -0,0 +1,14 @@
+import React, { FunctionComponent }  from "react";
+import ReactDOM from 'react-dom';
+
+type CardsProps = {
+    table: string,
+    fields: []
+}
+
+export const Card: FunctionComponent <CardsProps> ({ table, fields }) =>{<div></div>}
+<h2>{ table }</h2>
+  
+
+</aside>
+}
---
+++ src/containers/MainContainer.tsx
@@ -0,0 +1,9 @@
+import React, { FunctionComponent }  from "react";
+import ReactDOM from 'react-dom';
+
+export const MainContainer: FunctionComponent = () => <div>
+  
+</div>
+
+
+
