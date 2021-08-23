92dfab66c885f1c86329af3875489d6d0f2a28cc
Damien Evans <dnyceboy@gmail.com>
2021-08-23 15:07:26 -0400

    receiving resolvers and types from graphql from the backend and displaying it in the sidebar

--- src/components/GraphQLSidebar.tsx
+++ src/components/GraphQLSidebar.tsx
@@ -5,26 +5,44 @@ type props = {
   isMenuOpen: boolean,
   tables: Array<string>,
   fields: Array<object>,
-  uri: string
+  data: igraphQLData
 }
 
 interface igraphQLData {
-  Resolvers: string[],
-  Types: string[]
+  Resolvers: string,
+  Types: string[],
+  Mutations: string 
 }
 
-export const GraphQLSidebar: FunctionComponent<props> = ({ tables, fields, isMenuOpen, onMenuToggle, uri}) => {
+export const GraphQLSidebar: FunctionComponent<props> = ({ tables, fields, isMenuOpen, onMenuToggle, data }) => {
   
   console.log(tables);
   console.log(fields);
-
-  const graphQLData: igraphQLData[] = [];
+  console.log(data);
+  const { Resolvers, Types} = data;
+  const typeArr = [];
+  const resArr = [];
+  for (let i = 0; i < Resolvers.length; i++) {
+    resArr.push(<li key={i}>{Resolvers[i]}</li>)
+  }
+  for (let i = 0; i < Types.length; i++) {
+    typeArr.push(<li key={i}>{Types[i]}</li>)
+  }
 
   return (
     <div onClick={onMenuToggle} className={`sidebar-menu ${isMenuOpen === true ? 'open' : ''}` }>
       <h1>Sidebar goes here</h1>
       <ul className='sidebar-list'>
-
+        <li>Resolvers</li>
+        <ul className='sidebar-list'>
+        {resArr}
+        </ul>
+      </ul>
+      <ul className='sidebar-list'>
+        <li>Types</li>
+        <ul className='sidebar-list'>
+        {typeArr}
+        </ul>
       </ul>
     </div>
   )
--- src/containers/VisualContainer.tsx
+++ src/containers/VisualContainer.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent, useState }  from "react";
+import React, { FunctionComponent, useState, useEffect }  from "react";
 import Visualizer from '../components/Visualizer';
 import { Navbar } from "../components/Navbar";
 import { GraphQLSidebar } from '../components/GraphQLSidebar';'../components/GraphQLSidebar';
@@ -9,8 +9,14 @@ type props = {
   uri: string
 }
 
+interface igraphQLData {
+  Resolvers: string[],
+  Types: string[]
+}
+
 export const VisualContainer: FunctionComponent<props> = ({ fields, tables, uri}) => {
   const [isMenuOpen, setMenuToOpen] = useState(false);
+  const [sideBarData, setSideBarData] = useState<any>({});
   /*
   toggleMenu() {
     this.setState({isMenuOpen: !this.state.isMenuOpen})
@@ -23,26 +29,31 @@ export const VisualContainer: FunctionComponent<props> = ({ fields, tables, uri}
 
   */
 
-  if(isMenuOpen){
+  useEffect(() => {
+
     fetch("http://localhost:3333/qltest", {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({URI: uri}),
+    method: 'POST',
+     headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({URI: uri}),
     })
     .then(response => response.json())
     .then(data => {
-      console.log('Success', data);
+      // console.log('Success', data);
+      setSideBarData(data);
     })
     .catch(error => console.log('Error', error));
-  }
+
+  }, []);
+
+  const { data } = sideBarData;
 
   return (
   <div>
     <Navbar isMenuOpen={isMenuOpen} onMenuToggle={() => setMenuToOpen(!isMenuOpen)} />
     <div className='container'>
-    {!isMenuOpen ? null : <GraphQLSidebar uri={uri} tables={tables} fields={fields} isMenuOpen={isMenuOpen} onMenuToggle={() => setMenuToOpen(!isMenuOpen)}/>}
+    {!isMenuOpen ? null : <GraphQLSidebar data={data} tables={tables} fields={fields} isMenuOpen={isMenuOpen} onMenuToggle={() => setMenuToOpen(!isMenuOpen)}/>}
     <Visualizer fields={fields} tables={tables}/>
     </div>
     
