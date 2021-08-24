12d677e29e8a650f4eea3976359ae900f78df9a1
Damien Evans <dnyceboy@gmail.com>
2021-08-24 12:39:03 -0400

    graphQLSidebar is now respnsive on and doesnt clip off the page

--- src/containers/VisualContainer.tsx
+++ src/containers/VisualContainer.tsx
@@ -50,7 +50,7 @@ export const VisualContainer: FunctionComponent<props> = ({ fields, tables, uri}
   const { data } = sideBarData;
 
   return (
-  <div>
+  <div className='VisualContainer'>
     <Navbar isMenuOpen={isMenuOpen} onMenuToggle={() => setMenuToOpen(!isMenuOpen)} />
     <div className='container'>
     {!isMenuOpen ? null : <GraphQLSidebar data={data} tables={tables} fields={fields} isMenuOpen={isMenuOpen} onMenuToggle={() => setMenuToOpen(!isMenuOpen)}/>}
